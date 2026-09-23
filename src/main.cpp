4772a73b4cebc9330cef4a2d30e6981578bb65e4
nikunjcodes-hub <nikunjsinghal82@gmail.com>
2026-09-23 17:20:10 +0530

    Implement character frequency counting

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-int main(){
-    cout<<"Huffman compressor"<<endl;
+
+int main() {
+
+    cout << "Huffman compressor" << endl;
+
+    string text;
+
+    cout << "Enter text: ";
+    cin >> text;
+
+    int count[26] = {0};
+
+    for(const char& ch : text) {
+        count[ch - 'a']++;
+    }
+
+    for(char i = 'a'; i <= 'z'; i++) {
+        if(count[i - 'a'] != 0) {
+            cout << i << ":" << count[i - 'a'] << endl;
+        }
+    }
+
     return 0;
 }
-
